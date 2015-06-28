cb42b827ff206b05bafefcea50ab1a2391300298
manmak <manmak@gmail.com>
2015-06-28 22:14:12 +0200

    Buscador de Preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -12,9 +12,12 @@ exports.load = function(req, res, next, quizId) {
 
 // GET /quizes
 exports.index = function(req, res) {
-	models.Quiz.findAll().then(function(quizes) {
-		res.render('quizes/index', {quizes: quizes});
-	}).catch(function(error) { next(error); })
+	var like = '%' + (req.query.search || '').replace(/\s+/g, '%') + '%';
+	models.Quiz.findAll(
+		{where: ["lower(pregunta) like lower(?)", like], order: 'pregunta ASC'}).then(
+			function(quizes) {
+				res.render('quizes/index', {quizes: quizes});
+	}).catch(function(error) {next(error);});
 };
 
 // GET /quizes/:id
